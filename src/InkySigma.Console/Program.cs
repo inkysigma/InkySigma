7d85714e13bf11d3ff2b728a63300501b987a4f5
Justin Ley <inkysigma@outlook.com>
2015-10-15 23:37:18 -0700

    Cleaned up the project and moved things around.

--- src/InkySigma.Console/Program.cs
+++ src/InkySigma.Console/Program.cs
@@ -1,36 +1,13 @@
 ﻿using System;
-using System.IO;
-using Microsoft.AspNet.Hosting;
-using Microsoft.Dnx.Runtime;
-using Microsoft.Framework.Configuration;
-using Microsoft.Framework.DependencyInjection;
-
+using System.Diagnostics;
 namespace InkySigma.Console
 {
     public class Program
     {
-        private readonly IServiceProvider _provider;
-
-        public Program(IServiceProvider provider)
-        {
-            _provider = provider;
-        }
-
         public void Main(string[] args)
         {
-            var tempBuilder = new ConfigurationBuilder().AddCommandLine(args);
-            var tempConfig = tempBuilder.Build();
-            var configFilePath = tempConfig["config"];
 
-            var appBasPath = _provider.GetRequiredService<IApplicationEnvironment>().ApplicationBasePath;
-            var builder = new ConfigurationBuilder(Path.Combine(appBasPath, "../InkySigma"));
-            var config = builder.Build();
-
-            var host = new WebHostBuilder(_provider, config).Build();
-
-            using (host.Start())
-            {
-            }
+            var webHost = Process.Start("dnx", "web");
         }
     }
 }
---
+++ src/InkySigma/Model/StandardResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace InkySigma.Model
+{
+    /// <summary>
+    /// This is a standard response for all of the controllers.
+    /// First note that the code represents an HTTP code representation
+    /// The message is a user friendly message
+    /// Information is any further information
+    /// </summary>
+    public class StandardResponse<T>
+    {
+        public bool Succeeded { get; set; }
+        public int Code { get; set; } = 200;
+        public string Message { get; set; }
+        public string Information { get; set; }
+        public T Payload { get; set; }
+
+        public static StandardResponse<T> Create(T payload)
+        {
+            return new StandardResponse<T>
+            {
+                Succeeded = true,
+                Payload = payload
+            };
+        } 
+    }
+}
